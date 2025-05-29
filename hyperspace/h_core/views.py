3555b80449c1634d792e9bf943b05a301977b940
suphicakir <suphicakir@gmail.com>
2025-05-29 21:29:15 +0300

    tempaltes klasörü oluşturuldu
    templates klasörü oluşturuldu. index.html sayfası ekrana basılıyor.

--- hyperspace/h_core/views.py
+++ hyperspace/h_core/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-# Create your views here.
-def index (request):
-    return HttpResponse ("Welcome to Hyperspace")
+def index(request):
+    return render (request, 'index.html')
